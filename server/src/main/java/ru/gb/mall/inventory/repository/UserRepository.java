6dcd31ed441d1e17268973d56a62b2cf4217a368
nxbeyxnd <unblocksteam@gmail.com>
2021-12-12 22:36:49 +0300

    repo fix

--- server/src/main/java/ru/gb/mall/inventory/repository/UserRepository.java
+++ server/src/main/java/ru/gb/mall/inventory/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package ru.gb.mall.inventory.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.gb.mall.inventory.entity.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     Optional<User> findUserByLogin(String login);
     Optional<User> findUserByLoginAndPassword(String login, String password);
 }
